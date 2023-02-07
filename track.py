f0fee5c9f5a33329571617fa14dad12c3cec2a25
YashasreePannuri <73391354+YashasreePannuri@users.noreply.github.com>
2023-02-07 18:43:43 +0000

    Add files via upload

---
+++ track.py
@@ -0,0 +1,39 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/sreel/Downloads"
+
+class FileEventHandler(FileSystemEventHandler):
+
+    def on_created(self,event):
+        print(f"Hey,{event.scr_path} has been created!")
+
+    def on_modified(self,event):
+        print(f"Hey,{event.scr_path} has been modified!")
+
+    def on_moved(self,event):
+        print(f"Hey,{event.scr_path} has been moved!")
+
+    def on_deleted(self,event):
+        print(f"Oops! Someone deleted {event.scr_path}")
+
+
+event_handler = FileEventHandler()
+observer = Observer()
+observer.schedule(event_handler, from_dir, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped!")
+    observer.stop()
